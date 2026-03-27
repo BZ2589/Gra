87e0eca2d6d8eda28a6cfe7d84ed554a9e8a7b34
BZ2589 <lhao81317@gmail.com>
2026-03-27 16:33:48 +0800

    exp

--- changedetection/models/Mamba_backbone.py
+++ changedetection/models/Mamba_backbone.py
@@ -74,6 +74,16 @@ class Backbone_VSSM(VSSM):
                 if 'downsample.norm' in k:
                     k = k.replace('downsample.norm', 'downsample.3')
                 
+                # Reshape flattened weights from some VMamba implementations
+                if 'dt_projs_weight' in k and len(v.shape) == 2:
+                    # v is [K * d_inner, dt_rank], we want [K, d_inner, dt_rank] where K=4
+                    v = v.view(4, -1, v.shape[-1])
+                if 'x_proj_weight' in k and len(v.shape) == 2:
+                    # v is [K * N, d_inner], we want [K, N, d_inner] where K=4
+                    v = v.view(4, -1, v.shape[-1])
+                if 'dt_projs_bias' in k and len(v.shape) == 1:
+                    v = v.view(4, -1)
+                
                 new_state_dict[k] = v
             state_dict = new_state_dict
                 
