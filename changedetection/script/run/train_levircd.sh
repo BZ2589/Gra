d3df0c3126a22c4bfddef18c5bb04aefe905bf2d
BZ2589 <lhao81317@gmail.com>
2026-03-29 10:56:16 +0800

    从tiny模型切换到base模型

--- changedetection/script/run/train_levircd.sh
+++ changedetection/script/run/train_levircd.sh
@@ -2,11 +2,11 @@ CUDA_VISIBLE_DEVICES=1 python changedetection/script/train_MambaBCD.py --dataset
                                 --batch_size 8 \
                                 --crop_size 256 \
                                 --max_iters 800000 \
-                                --model_type baseline_tiny_levir_drop=0 \
+                                --model_type baseline_base_levir_drop=0 \
                                 --model_param_path 'changedetection/saved_models' \
                                 --train_dataset_path '/home/z/dataset/LEVIR-CD-1024/train' \
                                 --test_dataset_path '/home/z/dataset/LEVIR-CD-1024/test' \
                                 --decoder_depth 4 \
-                                --cfg './changedetection/configs/vssm1/vssm_tiny_224.yaml' \
-                                --pretrained_weight_path './changedetection/pretrained_weight/model.safetensors' \
+                                --cfg './changedetection/configs/vssm1/vssm_base_224.yaml' \
+                                --pretrained_weight_path './changedetection/pretrained_weight/vssmbase_0229_ckpt_epoch_236.pth' \
                                 --train_name "$1"
